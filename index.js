83be945cbc1364b60659a5c57a148fa88cf3f364
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-05-27 20:52:18 +0200

    Added genres and GET genres

--- index.js
+++ index.js
@@ -13,46 +13,84 @@ app.use(morgan('common')); // Logging requests using Morgan
 let movies = [
   {
     title: 'Forrest Gump',
-    director: 'Robert Zemeckis'
+    director: 'Robert Zemeckis',
+    genre: 'Drama'
   },
   {
     title: 'Lord of the Rings',
-    director: 'Peter Jackson'
+    director: 'Peter Jackson',
+    genre: 'Fantasy'
   },
   {
     title: 'Legends of the Fall',
-    director: 'Edward Zwick'
+    director: 'Edward Zwick',
+    genre: 'Drama'
+    }
   },
   {
     title: 'Braveheart',
-    director: 'Mel Gibson'
+    director: 'Mel Gibson',
+    genre: 'Historical fiction'
   },
   {
     title: 'Gladiator',
-    director: 'Ridley Scott'
+    director: 'Ridley Scott',
+    genre: 'Historical fiction'
+    }
   },
   {
     title: 'Kingdom of Heaven',
-    director: 'Ridley Scott'
+    director: 'Ridley Scott',
+    genre: 'Historical fiction'
+    }
   },
   {
     title: 'Troy',
-    director: 'Wolfgang Petersen'
+    director: 'Wolfgang Petersen',
+    genre: 'Historical drama'
+    }
   },
   {
     title: 'Fly Away Home',
-    director: 'Carroll Ballard'
+    director: 'Carroll Ballard',
+    genre: 'Drama'
+    }
   },
   {
     title: 'Last Vegas',
-    director: 'Jon Turteltaub'
+    director: 'Jon Turteltaub',
+    genre: 'Comedy'
   },
   {
     title: 'Some Like It Hot',
-    director: 'Billy Wilder'
+    director: 'Billy Wilder',
+    genre: 'Comedy'
+    }
   }
 ];
 
+let genres = [
+  {
+    name: 'Drama',
+    description: 'Within film, television and radio, drama is a genre of narrative fiction...'
+  },
+  {
+    name: 'Fantasy',
+    description: 'A fantasy story is about magic or supernatural forces...'
+  },
+  {
+    name: 'Historical fiction',
+    description: 'The genre historical fiction includes stories that are about the past...'
+  },
+  {
+    name: 'Historical drama',
+    description: 'A work set in a past time period...'
+  },
+  {
+    name: 'Comedy',
+    description: 'Comedy is a story that tells about a series of funny, or comical events...'
+  }
+];
 
 // Message upon hitting the root folder / home
 app.get('/', (req, res) => {
@@ -71,7 +109,10 @@ app.get('/movies/:title', (req, res) => {
 });
 
 // Returns data about a genre
-
+app.get('/movies/:genres/:name', (req, res) => {
+  res.json(genres.find((genre) =>
+    { return genre.name === req.params.name  }));
+});
 
 
 
