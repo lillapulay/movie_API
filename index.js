ce2563d937fc90ee493a25f5f41cc0cfc437cbf3
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-06-06 13:01:34 +0200

    Incorporated auth. to GET all movies function

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ app.get('/', (req, res) => {
 });
 
 // GET all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false}), (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
     });
 });
 
