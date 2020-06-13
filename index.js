5a80dabaf71e04cc47e65a30b36b760c40c872da
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-06-13 20:29:57 +0200

    Fixed CORS (all origins)

--- index.js
+++ index.js
@@ -24,21 +24,6 @@ let auth = require('./auth')(app); // Needs to be after the bodyParser middlewar
 const passport = require('passport');
 require('./passport');
 
-/* CORS list of allowed domains - commenting it out for now, all domains allowed
-let allowedOrigins = ['*']; // 
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-*/
-
 // Message upon hitting the root folder / home
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
