abcd771d7d726b84a7d3d77970536c3351ed89d0
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-06-14 15:34:24 +0200

    Integrated prop-types

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onClick } = this.props;
 
     return (
-      <div onClick={() => onClick(movie)} className="movie-card">
-        <p /><img className="movie-poster" src={movie.ImagePath} />
-        <br />
-        {movie.Title}
-        <br />
-        {movie.Description}
-      </div>
+      <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
