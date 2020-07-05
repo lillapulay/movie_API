ce014dc0b8a8dc297a4bd473db4b918da93d8b5b
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-07-05 21:18:07 +0200

    Built visibility-filter-input

---
+++ client/src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Form from 'react-bootstrap/Form';
+
+import { setFilter } from '../../actions/actions';
+
+// It has no state and doesn’t need lifecycle hooks, so doesn't have to be a class component
+// It's basically a wrapper around a React Bootstrap text imput + connected to the store
+// The VisibilityFilterInput component will be used as a sub-component of MoviesList
+function VisibilityFilterInput(props) {
+  return <Form.Control
+    onChange={e => props.setFilter(e.target.value)}
+    value={props.visibilityFilter}
+    placeholder="filter"
+  />;
+}
+
+export default connect(
+  null,
+  { setFilter }
+)(VisibilityFilterInput);
