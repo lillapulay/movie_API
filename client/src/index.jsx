8195be23e3392983136d7a911af17333ccc1f3d7
lillapulay <59831508+lillapulay@users.noreply.github.com>
2020-07-05 14:32:58 +0200

    Created store, v1

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import { MainView } from './components/main-view/main-view';
+import moviesApp from './reducers/reducers'; // Imports the reducer ( state + action = new state )
 
 // Import statement to indicate that we need to bundle `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp); // Store created in the topmost file of the application -> passed to all other views + components
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      // Wraps the entire app in a provider -> store is accessible from the entire app
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
   }
 }
 
